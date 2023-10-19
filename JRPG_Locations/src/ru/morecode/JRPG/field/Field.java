1ab344fc8e96264104fbcf4ee7bdb20884275bd5
Semyon Konovalov <semyon_konovalov_sw@mail.ru>
2023-10-19 12:06:52 +0300

    Добавлены классы для генерации деревни

---
+++ JRPG_Locations/src/ru/morecode/JRPG/field/Field.java
@@ -0,0 +1,8 @@
+package ru.morecode.JRPG.field;
+
+public abstract class Field {
+    protected int size;
+    public Field(int size){
+        this.size = size;
+    }
+}
---
+++ JRPG_Locations/src/ru/morecode/JRPG/location/VillageLocation.java
@@ -0,0 +1,61 @@
+package ru.morecode.JRPG.location;
+
+import ru.morecode.JRPG.field.VillageField;
+
+import java.io.*;
+
+public class VillageLocation extends Location {
+
+    VillageField[][] village;
+    public VillageLocation(int row, int col) {
+        super(row, col);
+        village = new VillageField[row][col];
+        String line;
+        int i = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("F:\\GIT\\JRPG\\JRPG_Locations\\src\\ru\\morecode\\JRPG\\VillageFile.txt"));
+            while((line = br.readLine()) != null){
+                String[] nameMas = line.split("\\|");
+                for (int j = 0; j < nameMas.length; j++){
+                    ///System.out.print(nameMas[j] + " ");
+                    String grass = "H";
+                    if (nameMas[j].equals(grass)){
+                        village[i][j] = new VillageField(10, true, false, false, grass);
+                        //village[i][j].printField();
+                    }
+                    String NPC = "N";
+                    if (nameMas[j].equals(NPC)){
+                        village[i][j] = new VillageField(10, false, true, false, NPC);
+                        //village[i][j].printField();
+                    }
+                    String labyrinth = "L";
+                    if (nameMas[j].equals(labyrinth)){
+                        village[i][j] = new VillageField(10, false, false, true, labyrinth);
+                        //village[i][j].printField();
+                    }
+                    String water = "W";
+                    if (nameMas[j].equals(water)) {
+                        village[i][j] = new VillageField(10, false, false, false, water);
+                        //village[i][j].printField();
+                    }
+                }
+                System.out.println();
+                i++;
+            }
+            br.close();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void printLocation(){
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                village[i][j].printField();
+            }
+            System.out.println();
+        }
+    }
+}
