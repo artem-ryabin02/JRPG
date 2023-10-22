733b40b4b4f91a291b94004750b5608edd924fd6
Semyon Konovalov <semyon_konovalov_sw@mail.ru>
2023-10-22 16:23:09 +0300

    Сделана реализация нажатия кнопки 'Начать новую игру', исправлен дизайн окон

--- JRPG/src/main/java/ru/morecode/jrpg/MainMenuController.java
+++ JRPG/src/main/java/ru/morecode/jrpg/MainMenuController.java
@@ -1,11 +1,18 @@
 package ru.morecode.jrpg;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import ru.morecode.jrpg.subclasses.SceneSizeChangeListener;
 
 public class MainMenuController {
 
@@ -24,6 +31,15 @@ public class MainMenuController {
     @FXML
     private Button buttonNewGame;
 
+    private void letterbox(final Scene scene, final Pane contentPane) {
+        final double initWidth  = scene.getWidth();
+        final double initHeight = scene.getHeight();
+        final double ratio      = initWidth / initHeight;
+
+        SceneSizeChangeListener sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth, contentPane);
+        scene.widthProperty().addListener(sizeListener);
+        scene.heightProperty().addListener(sizeListener);
+    }
     @FXML
     void initialize() {
         buttonExit.setOnAction(actionEvent ->{
@@ -33,7 +49,24 @@ public class MainMenuController {
             System.out.println("Load game");
         } );
         buttonNewGame.setOnAction(actionEvent ->{
-            System.out.println("New game");
+            buttonNewGame.getScene().getWindow().hide();
+
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("game-window.fxml"));
+            Stage stage = new Stage();
+            Pane root;
+            try {
+                root = fxmlLoader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            Scene scene = new Scene(new Group(root));
+            stage.setScene(scene);
+            stage.setTitle("Game window");
+            stage.show();
+            stage.setFullScreenExitHint("");
+            letterbox(scene, root);
+            stage.setFullScreen(true);
         } );
     }
 
