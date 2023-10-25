100ea78d7fd9bedac1d06be4b1988e508a6d299e
Semyon Konovalov <semyon_konovalov_sw@mail.ru>
2023-10-25 20:53:04 +0300

    теперь для запуска сцен реализован отдельный класс

--- JRPG/src/main/java/ru/morecode/jrpg/HelloApplication.java
+++ JRPG/src/main/java/ru/morecode/jrpg/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import ru.morecode.jrpg.subclasses.SceneSizeChangeListener;
+import ru.morecode.jrpg.subclasses.StartScene;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -14,41 +15,12 @@ import java.sql.*;
 import java.util.Properties;
 
 public class HelloApplication extends Application {
-    public static final String PATH_TO_PROPERTIES = "JRPG/src/main/resources/config.properties";
-
-    // если раскомментировать этот код, то программа запустится
-    private void letterbox(final Scene scene, final Pane contentPane) {
-        final double initWidth = scene.getWidth();
-        final double initHeight = scene.getHeight();
-        final double ratio = initWidth / initHeight;
-
-        SceneSizeChangeListener sizeListener = new SceneSizeChangeListener(scene, ratio, initHeight, initWidth, contentPane);
-        scene.widthProperty().addListener(sizeListener);
-        scene.heightProperty().addListener(sizeListener);
-    }
+    public static final String PATH_TO_PROPERTIES = "src/main/resources/config.properties";
 
     @Override
     public void start(Stage stage) {
-        // если раскомментировать этот код, то программа запустится
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(getClass().getResource("main-menu.fxml"));
-
-        Pane root;
-        try {
-            root = fxmlLoader.load();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        Scene scene = new Scene(new Group(root));
-        stage.setScene(scene);
-        stage.setTitle("Main Menu");
-        stage.show();
-        stage.setFullScreenExitHint("");
-        letterbox(scene, root);
-        stage.setFullScreen(true);
-
-//        StartScene scene = new StartScene(stage, "main-menu.fxml");
-//        scene.start();
+        StartScene scene = new StartScene( "main-menu.fxml");
+        scene.start();
 
 
     }
